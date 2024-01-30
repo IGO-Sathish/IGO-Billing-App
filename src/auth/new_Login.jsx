8b9b8fe1e79c0ca35a8a6decf7ba622aecf1721e
IGO-Sathish <156909449+IGO-Sathish@users.noreply.github.com>
2024-01-30 10:21:42 +0530

    signup to signin (Error)

--- src/auth/new_Login.jsx
+++ src/auth/new_Login.jsx
@@ -21,7 +21,7 @@ const new_Login = () => {
                             </div>
 
                             <div className="signin-form">
-                                <h2 className="form-title">Sign up</h2>
+                                <h2 className="form-title">Sign In</h2>
                                 <form method="POST" className="register-form" id="login-form">
                                     <div className="form-group">
                                         <label for="email"><MdEmail /></label>
